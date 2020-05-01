809459d7ba73f865ea5543c20ba1d328632b9cd5
Debojit Das <debojit666@gmail.com>
2020-05-01 03:45:27 +0200

    product list done without cart button

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import './Product.css'
 
 const Product = (props) => {
     console.log(props.product);
+    const {img, name, seller, price, stock} = props.product;
     return (
-        <div>
-            <h3>This is Product</h3>
-    <h4>{props.product.name}</h4>
+        <div className="product">
+            <div>
+                <img src={img} alt=""/>
+            </div>
+            <div>
+                <h4 className="product-name">{name}</h4>
+                <br/>
+                <p><small>by: {seller}</small></p>
+                <p>€{price}</p>
+                <br/>
+                <p><small>Only {stock} left in stock - Order Soon</small></p>
+            </div>
         </div>
     );
 };
